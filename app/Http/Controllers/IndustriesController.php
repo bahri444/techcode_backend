006cd685583e761299070cad1cc1169342cd2de7
bahri444 <bahrysaipul266@gmail.com>
2023-07-21 00:22:35 +0800

    update student class controller

--- app/Http/Controllers/IndustriesController.php
+++ app/Http/Controllers/IndustriesController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Actifity;
 use App\Models\Industries;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\Validator;
 
 class IndustriesController extends Controller
@@ -55,8 +57,63 @@ class IndustriesController extends Controller
     }
     public function UpdateIndustries(Request $request, $uuid)
     {
+        Validator::make($request->all(), [
+            'industry_name' => 'required',
+            'industy_logo' => 'required|image|mimes:png,svg,jpeg|max:2048',
+            'vision' => 'required ',
+            'mision' => 'required ',
+            'objective' => 'required',
+            'social_media' => 'required',
+        ]);
+        // dd($request);
+        try {
+            if ($request == true) {
+                $industry = Industries::find($uuid);
+
+                // handle file yang du upload
+                $getLogoUpload = $request->file('industy_logo');
+                $getLogoName = $getLogoUpload->hashName();
+                $saveIndirektory = "/industries_logo/$getLogoName";
+                $request->industy_logo->move(public_path('/industries_logo/'), $getLogoName);
+
+                // delete file lama
+                Storage::delete('/industries_logo/' . basename($industry->industy_logo));
+
+                // update data dengan logo yang baru di upload
+                $industry->industry_name = $request->input('industry_name');
+                $industry->industy_logo = $saveIndirektory;
+                $industry->vision = $request->input('vision');
+                $industry->mision = $request->input('mision');
+                $industry->objective = $request->input('objective');
+                $industry->social_media = $request->input('social_media');
+                $industry->save();
+                return response()->json([
+                    'success' => 'data berhasil di update',
+                    'data' => $industry
+                ], 200);
+            } elseif ($request == false) {
+                return response()->json([
+                    'errors' => "field tidak boleh kosong",
+                ], 400);
+            }
+        } catch (\Exception $e) {
+            return response()->json([
+                'errors' => "data gagal di update", $e
+            ], 500);
+        }
     }
     public function DeleteIndustries($uuid)
     {
+        try {
+            $industry = Industries::find($uuid);
+            $industry->delete();
+            return response()->json([
+                'errors' => 'data berhasil di hapus'
+            ], 200);
+        } catch (\Exception $e) {
+            return response()->json([
+                'errors' => 'data gagal di hapus', $e
+            ], 500);
+        }
     }
 }
--- app/Http/Controllers/StudentClassController.php
+++ app/Http/Controllers/StudentClassController.php
@@ -4,26 +4,93 @@ namespace App\Http\Controllers;
 
 use App\Models\StudentsClass;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class StudentClassController extends Controller
 {
     public function GetAllStudentClass()
     {
         $data = StudentsClass::with('joinToClass', 'joinToUser', 'joinToClassWithModul', 'joinToProfessionWithClass')->get();
-        return response()->json([
-            "data_transaction_class" => $data
-        ]);
+        return response()->json($data);
     }
     public function GetStudentClassByUuid($uuid)
     {
+        $findUuid = StudentsClass::with('joinToClass', 'joinToUser', 'joinToClassWithModul', 'joinToProfessionWithClass')->findOrFail($uuid);
+        $findUuid->student_class_uuid = $uuid;
+        return response()->json($findUuid);
     }
     public function AddStudentClass(Request $request)
     {
+        Validator::make(
+            $request->all(),
+            [
+                // student_class_uuid	user_uuid	class_uuid	date_checkout_class	payment_state
+                'user_uuid' => 'required',
+                'class_uuid' => 'required',
+                'date_checkout_class' => 'required',
+                'payment_state' => 'required',
+            ]
+        );
+        try {
+            $data = new StudentsClass([
+                // 'student_class_uuid' => $request->student_class_uuid,
+                'user_uuid' => $request->user_uuid,
+                'class_uuid' => $request->class_uuid,
+                'date_checkout_class' => $request->date_checkout_class,
+                'payment_state' => $request->payment_state,
+            ]);
+            return response()->json([
+                'success' => 'data berhasil di tambahkan',
+                'data' => $data
+            ], 200);
+        } catch (\Exception $e) {
+            return response()->json([
+                'errors' => 'data gagal di tambahkan', $e
+            ], 500);
+        }
     }
     public function UpdateStudentClass(Request $request, $uuid)
     {
+        Validator::make(
+            $request->all(),
+            [
+                'user_uuid' => 'required',
+                'class_uuid' => 'required',
+                'date_checkout_class' => 'required',
+                'payment_state' => 'required',
+            ]
+        );
+        try {
+            $data = StudentsClass::find($uuid);
+            $data->user_uuid = $request->input('user_uuid');
+            $data->class_uuid = $request->input('class_uuid');
+            $data->date_checkout_class = $request->input('date_checkout_class');
+            $data->payment_state = $request->input('payment_state');
+            $data->save();
+            return response()->json([
+                'success' => 'data berhasil di update',
+                'data' => $data
+            ]);
+        } catch (\Exception $e) {
+            return response()->json([
+                'errors' => 'data gagal di update',
+                $data
+            ]);
+        }
     }
     public function DeleteStudentClass($uuid)
     {
+        try {
+            $dataByUuid = StudentsClass::find($uuid);
+            $dataByUuid->delete();
+            return response()->json([
+                'success' => 'data berhasil di hapus',
+            ]);
+        } catch (\Exception $e) {
+            return response()->json([
+                'success' => 'data gagal di hapus',
+                $e
+            ]);
+        }
     }
 }
